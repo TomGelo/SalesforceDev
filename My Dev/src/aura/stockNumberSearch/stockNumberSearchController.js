af879cced5d08b8b4f7223bd2dc4943fa1a73e96
tom.gelo <tom.gelo@gmail.com>
2016-10-25 17:10:00 -0400

    hide/show kurt loader

--- My Dev/src/aura/stockNumberSearch/stockNumberSearchController.js
+++ My Dev/src/aura/stockNumberSearch/stockNumberSearchController.js
@@ -2,18 +2,22 @@
     searchKeyChange: function(component, event, helper) {
         var myEvent = $A.get("e.c:stockNumberSearchKeyChange"); 
         var spinner = component.find('spinner');
-         $A.util.addClass(spinner, 'slds-show');
         
-        if (event.getParams().keyCode === 13){
-        	myEvent.setParams({"searchKey": component.find("search").get("v.value")});
-        	myEvent.fire();
-    	}
-        
+         if (event.getParams().keyCode === 13){
+        	 $A.util.toggleClass(spinner, 'slds-hide');
+        	 setTimeout(function(){
+        	 
+        		  var spinnerShow = component.find('spinner');
+        		  $A.util.toggleClass(spinnerShow, 'slds-hide');
+        		  myEvent.setParams({"searchKey": component.find("search").get("v.value")});
+        		  myEvent.fire();
+        		 
+         	 }, 2000);
+         }
     },
     
     handleSearchKeyChange: function(component, event, helper){
         var icon = component.find('icon');
-        //$A.util.removeClass(icon, 'slds-hide');
-        //$A.util.addClass(icon, 'slds-show');
+        $A.util.toggleClass(icon, 'slds-hide');
     }
 })
